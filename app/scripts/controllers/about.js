dd63e3e0d749cc8b4abdf60744c60a3aa5484d21
Amrith Yerramilli <amrith.y@deltax.com>
2016-02-17 21:24:24 +0530

    initial commit
    default starter app generated by yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angGeocoderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angGeocoderApp
+ */
+angular.module('angGeocoderApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angGeocoderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angGeocoderApp
+ */
+angular.module('angGeocoderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

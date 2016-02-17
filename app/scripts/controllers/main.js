c91db900e9b2d8e79486285229a5f836b7713390
Amrith Yerramilli <amrith.y@deltax.com>
2016-02-18 00:43:17 +0530

    added a fileReader directive, basic skeleton of controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,45 @@
  * Controller of the angGeocoderApp
  */
 angular.module('angGeocoderApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function($scope, $log) {
+
+        function validateSchema(rows) {
+            var isValid = true;
+            var requiredProperties = ['id', 'houseNumber', 'streetName', 'city', 'country'];
+
+            angular.forEach(rows, function(value, key) {
+                value.isValid = true;
+                value.validationMessage = '';
+
+                for (var i = 0; i < requiredProperties.length; i++) {
+                    var requiredProperty = requiredProperties[i];
+                    if (!value[requiredProperty]) {
+                        value.isValid = false;
+                        value.validationMessage = requiredProperty + ' is required';
+                        isValid = false;
+                        break;
+                    }
+                }
+            });
+
+            return isValid;
+        }
+
+        function uploadFile() {
+            if (validateSchema($scope.parsedCsv)) {
+                $log.info('validation successful. navigating to details page.')
+            }
+        }
+
+        $scope.parsedCsv = [];
+        $scope.uploadedFile = '';
+        $scope.uploadEnabled = function() {
+            // check if file has been uploaded
+            return true;
+        }
+
+        $scope.uploadFile = uploadFile;
+        
+        
+
+    });
---
+++ app/scripts/directives/filereader.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name angGeocoderApp.directive:fileReader
+ * @description
+ * # fileReader
+ */
+angular.module('angGeocoderApp')
+    .directive('fileReader', function() {
+        return {
+            restrict: 'A',
+            scope: {
+                fileReader: "="
+            },
+            link: function postLink(scope, element, attrs) {
+                element.bind("change", function(changeEvent) {
+                    var reader = new FileReader();
+                    reader.onload = function(loadEvent) {
+                        scope.$apply(function() {
+                            scope.fileReader = loadEvent.target.result;
+                        });
+                    }
+                    reader.readAsText(changeEvent.target.files[0]);
+                });
+            }
+        };
+    });

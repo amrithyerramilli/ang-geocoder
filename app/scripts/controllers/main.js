736024161b93fbe1a9db5efa78f256864b8a3151
Amrith Yerramilli <amrith.y@deltax.com>
2016-02-18 23:13:34 +0530

    render table for parsed csv

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,21 +8,22 @@
  * Controller of the angGeocoderApp
  */
 angular.module('angGeocoderApp')
-    .controller('MainCtrl', function($scope, $log) {
+    .controller('MainCtrl', function($scope, $log, $rootScope) {
 
         function validateSchema(rows) {
             var isValid = true;
-            var requiredProperties = ['id', 'houseNumber', 'streetName', 'city', 'country'];
+            // var requiredProperties = ['id', 'houseNumber', 'streetName', 'city', 'country'];
+            var requiredProperties = [0, 3, 4, 5, 6];
 
             angular.forEach(rows, function(value, key) {
-                value.isValid = true;
-                value.validationMessage = '';
+                value[7] = true;
+                value[8] = '';
 
                 for (var i = 0; i < requiredProperties.length; i++) {
                     var requiredProperty = requiredProperties[i];
                     if (!value[requiredProperty]) {
-                        value.isValid = false;
-                        value.validationMessage = requiredProperty + ' is required';
+                        value[7] = false;
+                        value[8] = 'field is required';
                         isValid = false;
                         break;
                     }
@@ -32,21 +33,30 @@ angular.module('angGeocoderApp')
             return isValid;
         }
 
-        function uploadFile() {
-            if (validateSchema($scope.parsedCsv)) {
-                $log.info('validation successful. navigating to details page.')
+        function processData(csv) {
+            var separator = ',';
+            var allTextLines = csv.split(/\r\n|\n/); // split on newline
+            var lines = [];
+            for (var i = 0; i < allTextLines.length; i++) {
+                var data = allTextLines[i].split(separator); // split CSV
+                lines.push(data);
             }
+            return lines;
         }
 
-        $scope.parsedCsv = [];
-        $scope.uploadedFile = '';
-        $scope.uploadEnabled = function() {
+        function uploadFile() {
+            $scope.parsedCsv = processData($scope.uploadedFile);
+            if (validateSchema($scope.parsedCsv)) {
+                $log.info('validation successful. navigating to details page.');
+                // convert each address to geocoded lat/lon
+            }            
+        }
+
+        $scope.uploadedFile = null;
+        $scope.uploadDisabled = function() {
             // check if file has been uploaded
-            return true;
+            return $scope.uploadedFile === null;
         }
 
         $scope.uploadFile = uploadFile;
-        
-        
-
     });

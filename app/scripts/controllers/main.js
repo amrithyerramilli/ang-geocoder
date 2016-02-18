48f67a119b0daecf44e468ae970b376a0af768ae
Amrith Yerramilli <amrith.y@deltax.com>
2016-02-19 04:37:32 +0530

    navigate to /details after upload

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,23 +8,28 @@
  * Controller of the angGeocoderApp
  */
 angular.module('angGeocoderApp')
-    .controller('MainCtrl', function($scope, $log, $rootScope, utils) {
+    .controller('MainCtrl', function($scope, $log, $rootScope, utils, $location) {
 
         function uploadFile() {
             $rootScope.detailsData = null;
             $scope.parsedCsv = utils.processData($scope.uploadedFile);
             if (utils.validateSchema($scope.parsedCsv)) {
 
-                $log.info('validation successful. generating geocoded lat/lon');
+                alert('validation successful. generating geocoded lat/lon');
+
                 utils.generateGeocodes($scope.parsedCsv)
                     .then(function() {
                         // all ok
                         $log.info('aal izz well, need to go to next step');
                         $rootScope.detailsData = $scope.parsedCsv;
+                        // alert("geolocation info successful, please go to details screen");
+                        $location.url("/details");
                     }, function(err) {
                         $log.error('oops, geocode fail macha');
                         $log.error(err);
                     });
+            } else {
+                alert('validation failed'); // TODO : display validation errors and stay on same page.
             }
         }
 

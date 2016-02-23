4f1babeb0380048c56927f157323c78bae323ed9
Amrith Yerramilli <amrith.y@deltax.com>
2016-02-23 22:48:10 +0530

    added test for utils.processData

--- test/spec/services/utils.js
+++ test/spec/services/utils.js
@@ -11,8 +11,14 @@ describe('Service: utils', function () {
     utils = _utils_;
   }));
 
-  it('should do something', function () {
+  it('utils must be valid', function () {
     expect(!!utils).toBe(true);
+  });
+
+  it('should parse the csv and return an array of 2 objects', function () {
+    var stringData = "Id,First Name,Last Name,House Number,Street Name,City,Country\n1,A,Y,B6,L&T South City,Bangalore,India\n2,S,Y,2,L&T,Bangalore,India";
+    var parsedCsv = utils.processData(stringData);
+    expect(parsedCsv.length).toBe(2);
   });
 
 });

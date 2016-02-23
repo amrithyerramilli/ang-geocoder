7f51e7194854929dffdd94298f5c64b96a379a35
Amrith Yerramilli <amrith.y@deltax.com>
2016-02-24 01:36:49 +0530

    added StackOverflow answer reference as a TODO

--- test/spec/directives/filereader.js
+++ test/spec/directives/filereader.js
@@ -1,20 +1,22 @@
-// 'use strict';
+'use strict';
 
-// describe('Directive: fileReader', function () {
+describe('Directive: fileReader', function () {
 
-//   // load the directive's module
-//   beforeEach(module('angGeocoderApp'));
+  // load the directive's module
+  beforeEach(module('angGeocoderApp'));
 
-//   var element,
-//     scope;
+  var element,
+    scope;
 
-//   beforeEach(inject(function ($rootScope) {
-//     scope = $rootScope.$new();
-//   }));
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
 
-//   // it('should make hidden element visible', inject(function ($compile) {
-//   //   element = angular.element('<file-reader></file-reader>');
-//   //   element = $compile(element)(scope);
-//   //   expect(element.text()).toBe('this is the fileReader directive');
-//   // }));
-// });
+  // TODO : https://github.com/matteosuppo/angular-filereader/blob/master/tests/filereader.service.js
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<file-reader></file-reader>');
+    element = $compile(element)(scope);
+    // expect(element.text()).toBe('this is the fileReader directive');
+  }));
+});
